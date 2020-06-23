fba6520a1646f12411f00df47ac6f8a907555c7c
Malik N Harris <54946887+malikharris@users.noreply.github.com>
2020-06-23 17:50:41 -0500

    feat: commands for index

--- bot/index.js
+++ bot/index.js
@@ -1,6 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const { token, prefix } = require("./config");
+const commands = require("./commands.json");
 
 client.on("ready", () => {
 	console.log(`Logged in as ${client.user.tag}!`);
@@ -13,6 +14,12 @@ client.on("message", msg => {
 		msg.reply(` you know who the best Nina is.`);
 	} else if (msg.content === `${prefix}coinToss`) {
 		msg.channel.send(Math.random() * 1 ? "Heads" : "Tails");
+	} else if (msg.content === `${prefix}commands`) {
+		let output = "Commands |  Description \n";
+		commands.map(cmd => {
+			output = `${output}${cmd.command}: ${cmd.description}\n`;
+		});
+		msg.channel.send(output);
 	}
 });
 
