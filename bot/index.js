733dca71386851176cd9756da5b37783eacfab1d
Vincent Bundage <vincentbundage@gmail.com>
2020-07-22 17:40:15 -0600

    Adjusting squad command

--- bot/index.js
+++ bot/index.js
@@ -84,7 +84,7 @@ client.on("message", msg => {
 			}
 			break;
 		case `${prefix}squad`:
-			msg.channel.send(`@Valorant`);
+			msg.channel.send(msg.member._roles);
 			// msg.channel.send(new Discord.MessageEmbed()
 			// 	.set("Let's get it!")
 			// 	.setImage("https://i.imgur.com/UCUjEWC.jpg"));
