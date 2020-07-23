2db4b8a45d1e8bb716dbe7ea23f56f32fabc9f02
Vincent Bundage <vincentbundage@gmail.com>
2020-07-22 18:01:50 -0600

    Adjusting squad command

--- bot/config.js
+++ bot/config.js
@@ -4,8 +4,8 @@ const config = {
 	prefix: "!",
 	token: process.env.CLIENT_TOKEN,
 	roles: {
-		"valorant": 725150520670552095,
-		"homies": 725073064471035976
+		"valorant": "725150520670552095",
+		"homies": "725073064471035976"
 	}
 };
 
