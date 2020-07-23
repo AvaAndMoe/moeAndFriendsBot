833d428fb9c6fa185e46dfc14e8dec1c776340b5
Vincent Bundage <vincentbundage@gmail.com>
2020-07-22 21:35:00 -0600

    Adding kick

--- bot/commands/index.js
+++ bot/commands/index.js
@@ -1,5 +1,6 @@
 const { unSubscribe, subscribe } = require("./subscribe");
 const commands = require("./commands");
 const eightBall = require("./eightBall");
+const kick = require("./kick");
 
-module.exports = { unSubscribe, subscribe, eightBall, listCommands: commands };
+module.exports = { unSubscribe, subscribe, eightBall, listCommands: commands, kick };
