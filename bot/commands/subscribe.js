48bb667dbd41b4c6ecea4f2dc8a92d9ad7450ecf
Malik N Harris <54946887+malikharris@users.noreply.github.com>
2020-07-22 22:48:14 -0500

    fix: ban and subscribe

--- bot/commands/subscribe.js
+++ bot/commands/subscribe.js
@@ -4,7 +4,7 @@ const validRole = (clan, member) => {
 
 const subscribe = (msg, subCommand) => {
 	switch (subCommand.toLowerCase()) {
-		case "valorant": {
+		case "valorant":
 			if (validRole("Valorant", msg.member)) {
 				msg.channel.send(`${msg.author} is already subscribed to Valorant`);
 			} else {
@@ -13,7 +13,15 @@ const subscribe = (msg, subCommand) => {
 				msg.channel.send(`${msg.author} is now subscribed to Valorant`);
 			}
 			break;
-		}
+		case "chess":
+			if (validRole("Chess", msg.member)) {
+				msg.channel.send(`${msg.author} is already subscribed to Chess`);
+			} else {
+				const role = msg.guild.roles.cache.find(role => role.name === "Chess");
+				msg.member.roles.add(role);
+				msg.channel.send(`${msg.author} is now subscribed to Chess`);
+			}
+			break;
 		default: {
 			msg.channel.send(`${msg.author} must select a valid role`);
 			break;
@@ -23,9 +31,8 @@ const subscribe = (msg, subCommand) => {
 
 const unSubscribe = (msg, subCommand) => {
 	switch (subCommand.toLowerCase()) {
-		case "valorant": {
+		case "valorant":
 			if (validRole("Valorant", msg.member)) {
-				const member = msg.member;
 				const role = msg.guild.roles.cache.find(role => role.name === "Valorant");
 				msg.member.roles.remove(role);
 				msg.channel.send(`${msg.author} is now unsubscribed to Valorant`);
@@ -33,7 +40,15 @@ const unSubscribe = (msg, subCommand) => {
 				msg.channel.send(`${msg.author} is not subscribed to Valorant`);
 			}
 			break;
-		}
+		case "chess":
+			if (validRole("Chess", msg.member)) {
+				const role = msg.guild.roles.cache.find(role => role.name === "Chess");
+				msg.member.roles.remove(role);
+				msg.channel.send(`${msg.author} is now unsubscribed to Chess`);
+			} else {
+				msg.channel.send(`${msg.author} is not subscribed to Chess`);
+			}
+			break;
 		default: {
 			msg.channel.send(`${msg.author} must select a valid role`);
 			break;
