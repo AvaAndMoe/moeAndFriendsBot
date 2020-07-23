589b195b40a528b6d95871f06918be3a0aa7633b
Malik N Harris <54946887+malikharris@users.noreply.github.com>
2020-07-22 22:48:14 -0500

    fix: ban and subscribe

--- bot/config.js
+++ bot/config.js
@@ -3,10 +3,6 @@ require("dotenv").config();
 const config = {
 	prefix: "!",
 	token: process.env.CLIENT_TOKEN,
-	roles: {
-		"valorant": "725150520670552095",
-		"homies": "725073064471035976"
-	}
 };
 
 module.exports = config;
